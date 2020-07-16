ba8d21e321e62351e9a6d2d9153db95759363ede
Eduardo Guette <eduardoguette@github.com>
2020-07-16 09:03:44 +0200

    more quick search 2

--- src/Components/QuickFinder.js
+++ src/Components/QuickFinder.js
@@ -3,7 +3,7 @@ import { Link } from "wouter";
 import "./QuickFinder.css"
 
 const populares = [
-  "Bailar","Molesto","Ay Dios Mio","MeGusta","Ayayai","De nada","Flojera","Estresado","Avergonzado","Aplausos","Te la creiste","Buena suerte","Chócala","Obvio","Aww","Aburrido","Loco","Loquesea","Suspiro","Besos","Buenas noches","Calma", "Llorar", "Si","No","Lo sieto","Con suerño","Hola","Abrazos","Vale","Por favor","Gracias","Guiño","Me vale","Sonrisa","Atónito","Feliz","Incómodo","Enfadado", "Asco", "Ups", "Increible", "Nervios", "Amor","Bailar", "Te extraño",
+  "Bailar","Molesto","Ay Dios Mio","Me Gusta","Ayayai","De nada","Flojera","Estresado","Avergonzado","Aplausos","Te la creiste","Buena suerte","Chócala","Obvio","Aww","Aburrido","Loco","Lo que sea","Suspiro","Besos","Buenas noches","Calma", "Llorar", "Si","No","Lo siento","Con sueño","Hola","Abrazos","Vale","Por favor","Gracias","Guiño","Me vale","Sonrisa","Atónito","Feliz","Incómodo","Enfadado", "Asco", "Ups", "Increible", "Nervios", "Amor","Bailar", "Te extraño",
 ]
 
 export default function QuickFinder() {
